6b97c0ecc3ecae5b5dec36ac305b3292af9e622b
Sumit Kar <sumkar@cisco.com>
2021-02-12 19:03:41 +0530

    added client id and secret

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  client_id: '',
+  client_secret: '',
+  redirect_uri: '',
+  scope: 'spark:all spark:kms'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  client_id: '',
+  client_secret: '',
+  redirect_uri: '',
+  scope: 'spark:all spark:kms'
 };
 
 /*
